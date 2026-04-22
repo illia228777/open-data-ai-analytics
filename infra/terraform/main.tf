2918bbb2fb026a7df23bb5c7e3362db9ed9564aa
Illia Matsko <leosmile06@gmail.com>
2026-04-22 11:23:09 +0300

    feat: update main.tf and cloud-init

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -37,21 +37,37 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
-        description = "Grafana"
-        from_port   = 3000
-        to_port     = 3000
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+   ingress {
+       description = "Grafana"
+       from_port   = 3000
+       to_port     = 3000
+       protocol    = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+   }
 
-    ingress {
-        description = "Prometheus"
-        from_port   = 9090
-        to_port     = 9090
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+   ingress {
+       description = "Prometheus"
+       from_port   = 9090
+       to_port     = 9090
+       protocol    = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+   }
+
+ingress {
+    description = "K8s Web App"
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Argo CD"
+    from_port   = 30443
+    to_port     = 30443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
